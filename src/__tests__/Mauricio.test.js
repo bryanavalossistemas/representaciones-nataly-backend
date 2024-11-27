9892e4699c285cd0f03fa9035a7b9f6ab408ba76
Mauri R <ramirezmauricio39@gmail.com>
2024-11-27 12:53:19 -0500

    correcciones test mau

--- src/__tests__/Mauricio.test.js
+++ src/__tests__/Mauricio.test.js
@@ -7,7 +7,7 @@ describe("Descripcion: Este test sirve para verificar el endpoint PUT /api/usuar
       nombre: "Mauricio",
       apellido: "Ramirez",
       direccion: "Avenida Canada 121",
-      celular: "91231244",
+      celular: "912312442",
       distritoId: 1,
     };
 
@@ -22,23 +22,21 @@ describe("Descripcion: Este test sirve para verificar el endpoint PUT /api/usuar
       nombre: "Mauricio",
       apellido: "Ramirez",
       direccion: "Avenida Canada 121",
-      celular: "91231244",
+      celular: "912312442",
       distritoId: 1,
       usuarioId: 1,
     });
 
     expect(response.body.data.id).toBeDefined();
   });
-});
 
 //prueba 2
-describe("Descripcion: Este test sirve para verificar el endpoint PUT /api/usuario/createOrUpdate", () => {
   it("Debe: crear o actyalizar la direccion del usuario", async () => {
     const crearOActualizar = {
       nombre: "Mauricio",
       apellido: "Ramirez",
       direccion: "Avenida Canada 121",
-      celular: "91231244",
+      celular: "912312442",
       distritoId: 59,
     };
 
@@ -46,61 +44,38 @@ describe("Descripcion: Este test sirve para verificar el endpoint PUT /api/usuar
       .put("/api/direcciones/usuario/createOrUpdate")
       .send(crearOActualizar);
 
-    expect(response.status).toBe(200);
-
-    expect(response.body.data).toEqual({
-      id: expect.any(Number),
-      nombre: "Mauricio",
-      apellido: "Ramirez",
-      direccion: "Avenida Canada 121",
-      celular: "91231244",
-      distritoId: 1,
-      usuarioId: 1,
-    });
-
-    expect(response.body.data.id).toBeDefined();
+    expect(response.status).toBe(404);
+    
+    expect(response.body.message).toBe("Error de distrito no encontrado")
   });
-});
 
 //Prueba 3
-describe("Descripcion: Este test sirve para verificar el endpoint PUT /api/usuario/createOrUpdate", () => {
   it("Debe: crear o actyalizar la direccion del usuario", async () => {
     const crearOActualizar = {
       nombre: "Mauricio",
       apellido: "Ramirez",
       direccion: "Avenida Canada 121",
       celular: "91231244",
-      distritoId: 59,
+      distritoId: 1,
     };
 
     const response = await request(app)
       .put("/api/direcciones/usuario/createOrUpdate")
       .send(crearOActualizar);
 
-    expect(response.status).toBe(200);
+    expect(response.status).toBe(404);
 
-    expect(response.body.data).toEqual({
-      id: expect.any(Number),
-      nombre: "Mauricio",
-      apellido: "Ramirez",
-      direccion: "Avenida Canada 121",
-      celular: "91231244",
-      distritoId: 1,
-      usuarioId: 1,
-    });
-
-    expect(response.body.data.id).toBeDefined();
+    expect(response.body.message).toBe("El numero debe tener 9 digitos exactos.")
   });
-});
+
 
 //Prueba 4
-describe("Descripcion: Este test sirve para verificar el endpoint PUT /api/usuario/createOrUpdate", () => {
   it("Debe: crear o actyalizar la direccion del usuario", async () => {
     const crearOActualizar = {
       nombre: "Mauricio324",
       apellido: "Ramirez",
       direccion: "Avenida Canada 121",
-      celular: "91231244",
+      celular: "912312442",
       distritoId: 1,
     };
 
@@ -108,18 +83,8 @@ describe("Descripcion: Este test sirve para verificar el endpoint PUT /api/usuar
       .put("/api/direcciones/usuario/createOrUpdate")
       .send(crearOActualizar);
 
-    expect(response.status).toBe(200);
-
-    expect(response.body.data).toEqual({
-      id: expect.any(Number),
-      nombre: "Mauricio",
-      apellido: "Ramirez",
-      direccion: "Avenida Canada 121",
-      celular: "91231244",
-      distritoId: 1,
-      usuarioId: 1,
-    });
-
-    expect(response.body.data.id).toBeDefined();
+    expect(response.status).toBe(404);
+    
+    expect(response.body.message).toBe("El nombre o apellido no deben contener ningun numero.")
   });
 });
